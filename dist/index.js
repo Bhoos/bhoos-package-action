e6a54d8c21077dde6a0369f8010e6337188fce5b
Bhoos Action <action@bhoos.com>
2020-09-03 14:32:00 +0545

    update index add push to branch

--- dist/index.js
+++ dist/index.js
@@ -3266,7 +3266,7 @@ try {
   console.log('girURl', gitURL);
   console.log(repository);
 
-  execSync(`curl -H 'Authorization: token ${PERSONAL_ACCESS_TOKEN}'`)
+  execSync(`curl -H 'Authorization: token ${PERSONAL_ACCESS_TOKEN}' ${html_url}`)
   execSync(`git clone ${gitURL}`)
   execSync(`echo -e "machine github.com\n  login ${PERSONAL_ACCESS_TOKEN}" >> ~/.netrc`)
   execSync(`git config --global user.email action@bhoos.com`);
